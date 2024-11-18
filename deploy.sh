dc11ecbcb1f9e1e2c4ba51a2dee926eb78232137
Natnael-Getnet <natnaelgetnetharegewoyn@gmail.com>
2024-11-18 08:17:35 +0300

    Added comment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker compose run --rm composer update
 # Install Fresh laravel using docker
-# For example i am installing the latest laravel versing docker. You can change this for your preferd installation version
+# For example i am installing the latest laravel version docker. You can change this for your preferd installation version
 docker compose run --rm composer laravel install laravel .
 docker compose run --rm npm run dev
 docker compose run --rm artisan migrate
